38b5bdb1a1fcd4a33f754f671b053974f64993ce
Wei Ji <23487320+weiji14@users.noreply.github.com>
2025-05-01 16:54:35 +1200

    Add test to check that FileNotFoundError is raised when file is missing

--- python/tests/test_cog.py
+++ python/tests/test_cog.py
@@ -1,7 +1,10 @@
 import async_tiff
 from time import time
+
+import pytest
+
 from async_tiff import TIFF
-from async_tiff.store import S3Store
+from async_tiff.store import LocalStore, S3Store
 
 store = S3Store("sentinel-cogs", region="us-west-2", skip_signature=True)
 path = "sentinel-s2-l2a-cogs/12/S/UF/2022/6/S2B_12SUF_20220609_0_L2A/B04.tif"
@@ -25,3 +28,12 @@ gkd.projected_type
 gkd.citation
 
 dir(gkd)
+
+
+async def test_cog_missing_file():
+    """
+    Ensure that a FileNotFoundError is raised when passing in a missing file.
+    """
+    store = LocalStore()
+    with pytest.raises(FileNotFoundError):
+        await TIFF.open(path="imaginary_file.tif", store=store)
