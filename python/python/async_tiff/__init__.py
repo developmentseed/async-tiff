5fd524e050dd4b7acdeb4d6b9ceda16ed9091a1d
Kyle Barron <kyle@developmentseed.org>
2026-01-23 16:32:28 -0500

    fix ruff warnings

--- python/python/async_tiff/__init__.py
+++ python/python/async_tiff/__init__.py
@@ -1,9 +1,33 @@
 from typing import TYPE_CHECKING
 
-from ._async_tiff import *
-from ._async_tiff import ___version
+from ._async_tiff import (
+    Array,
+    Decoder,
+    DecoderRegistry,
+    GeoKeyDirectory,
+    ImageFileDirectory,
+    ThreadPool,
+    TIFF,
+    ObspecInput,
+    Tile,
+)
+
+from ._async_tiff import ___version  # noqa: F403 # pyright:ignore[reportAttributeAccessIssue]
 
 if TYPE_CHECKING:
     from . import store
 
 __version__: str = ___version()
+
+__all__ = [
+    "store",
+    "Array",
+    "Decoder",
+    "DecoderRegistry",
+    "GeoKeyDirectory",
+    "ImageFileDirectory",
+    "ThreadPool",
+    "TIFF",
+    "ObspecInput",
+    "Tile",
+]
--- python/python/async_tiff/_async_tiff.pyi
+++ python/python/async_tiff/_async_tiff.pyi
@@ -1,9 +1,21 @@
-from ._array import Array as Array
-from ._decoder import Decoder as Decoder
-from ._decoder import DecoderRegistry as DecoderRegistry
-from ._geo import GeoKeyDirectory as GeoKeyDirectory
-from ._ifd import ImageFileDirectory as ImageFileDirectory
-from ._thread_pool import ThreadPool as ThreadPool
-from ._tiff import TIFF as TIFF
-from ._tiff import ObspecInput as ObspecInput
-from ._tile import Tile as Tile
+from ._array import Array
+from ._decoder import Decoder
+from ._decoder import DecoderRegistry
+from ._geo import GeoKeyDirectory
+from ._ifd import ImageFileDirectory
+from ._thread_pool import ThreadPool
+from ._tiff import TIFF
+from ._tiff import ObspecInput
+from ._tile import Tile
+
+__all__ = [
+    "Array",
+    "Decoder",
+    "DecoderRegistry",
+    "GeoKeyDirectory",
+    "ImageFileDirectory",
+    "ThreadPool",
+    "TIFF",
+    "ObspecInput",
+    "Tile",
+]
