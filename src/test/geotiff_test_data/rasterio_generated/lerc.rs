8b7a8fb87923b0aa817777e5ac9c6bbfbaa784ae
Kyle Barron <kyle@developmentseed.org>
2026-02-02 22:54:00 -0500

    add test

---
+++ src/test/geotiff_test_data/rasterio_generated/lerc.rs
@@ -0,0 +1,21 @@
+use crate::test::util::open_tiff;
+use crate::{DataType, TypedArray};
+
+#[tokio::test]
+async fn test_lerc() {
+    let filename = "geotiff-test-data/rasterio_generated/fixtures/float32_1band_lerc_block32.tif";
+    let (reader, tiff) = open_tiff(filename).await;
+    let ifd = &tiff.ifds()[0];
+    assert_eq!(ifd.image_height(), 128);
+    assert_eq!(ifd.image_width(), 128);
+    assert_eq!(ifd.tile_width(), Some(64));
+    assert_eq!(ifd.tile_height(), Some(64));
+
+    let tile: crate::Tile = ifd.fetch_tile(0, 0, &reader).await.unwrap();
+
+    let array = tile.decode(&Default::default()).unwrap();
+
+    assert_eq!(array.shape, [64, 64, 1]);
+    assert_eq!(array.data_type, Some(DataType::Float32));
+    assert!(matches!(array.data, TypedArray::Float32(_)));
+}
